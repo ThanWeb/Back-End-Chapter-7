8d842b02b9a162143266543dc3b706af0fcc113e
Hans <classsickpeople@gmail.com>
2023-05-31 16:54:38 +0800

    Create testing for handleGetCar in CarController

--- app/controllers/CarController.spec.js
+++ app/controllers/CarController.spec.js
@@ -15,11 +15,11 @@ describe('#handleListCars', () => {
         pageCount: 2,
         pageSize: 1,
         count: 2
-    }
+      }
     }
 
     for (let i = 0; i < 2; i++) {
-      const car = new Car({ name, price, size });
+      const car = new Car({ id: i + 1, name, price, size });
       cars.push(car);
     }
 
@@ -36,5 +36,47 @@ describe('#handleListCars', () => {
     expect(mockCarModel.findAll).toHaveBeenCalled();
     expect(mockResponse.status).toHaveBeenCalledWith(200);
     expect(mockResponse.json).toHaveBeenCalledWith({cars, meta});
+  });
+});
+
+describe('#handleGetCar', () => {
+  it('should call res.status(200) and res.json spesific car', async () => {
+    const cars = []
+    const carsData = [
+      {
+        id: 1,
+        name: "Duster 360",
+        price: 300000,
+        size: "MEDIUM",
+      },
+      {
+        id: 2,
+        name: "Duster 360",
+        price: 300000,
+        size: "MEDIUM",
+      }
+    ];
+
+    for (let i = 0; i < carsData.length; i++) {
+      const car = new Car({ id: i + 1, name: carsData[i].name, price: carsData[i].price, size: carsData[i].size });
+      cars.push(car);
+    }
+
+    const mockRequest = {
+      params: {
+        id: 1
+      }
+    }
+    const mockCarModel = { findByPk: jest.fn().mockReturnValue(cars[0]) };
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockReturnThis(),
+    };
+
+    const carController = new CarController({ carModel: mockCarModel });
+    await carController.handleGetCar(mockRequest, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(mockResponse.json).toHaveBeenCalledWith(cars[0]);
   });
 });
